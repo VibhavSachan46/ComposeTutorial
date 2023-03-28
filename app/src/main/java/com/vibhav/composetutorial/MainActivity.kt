a26d8543b19f83c07b457b72fa53f6c205bca50f
Vibhav <vibhavsachan743@gmail.com>
2023-03-28 10:02:46 +0530

    Button click

--- app/src/main/java/com/vibhav/composetutorial/MainActivity.kt
+++ app/src/main/java/com/vibhav/composetutorial/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -28,21 +30,45 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                var count by remember {
+                    mutableStateOf(0)
+                }
+//                    Greeting("Vibhav")
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Vibhav")
-
+                    Text(
+                        text = count.toString(),
+                        fontSize = 30.sp
+                    )
+                    Button(onClick = {
+                        count++
+                    }) {
+                        Text(text = "click me")
+                    }
                 }
             }
         }
     }
 }
+//
+//@Composable
+//fun Greeting(name: String) {
+//    LazyRow(modifier = Modifier.fillMaxSize()) {
+//        items(10)
+//        {
+//            Icon(
+//                imageVector = Icons.Default.Add,
+//                contentDescription = null,
+//                modifier = Modifier.size(100.dp)
+//            )
+//        }
+//    }
+    
 
-@Composable
-fun Greeting(name: String) {
+    
 //    Image(
 //        painter = painterResource(id = R.drawable.ic_launcher_foreground),
 //        contentDescription = null,
@@ -103,12 +129,11 @@ fun Greeting(name: String) {
 //        )
 //    }
 
-}
+//}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    ComposeTutorialTheme {
+//    }
+//}
